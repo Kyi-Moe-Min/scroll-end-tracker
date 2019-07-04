994d6e336749bf92e04a5f4e400cbae8dc539cc6
Kyi-Moe-Min <34179716+Kyi-Moe-Min@users.noreply.github.com>
2019-07-04 12:01:21 +0630

    for scroll end

--- src/app.js
+++ src/app.js
@@ -4,12 +4,23 @@ export default class extends React.Component {
   componentDidMount() {
     this.ss = document.getElementById("ss");
   }
-
+  timeouts = [];
   scrolled = e => {
     const { offsetHeight, scrollTop, scrollHeight } = this.ss;
-    console.log(offsetHeight + scrollTop, scrollHeight);
-    if (offsetHeight + scrollTop === scrollHeight)
-      console.log("it's at the end.");
+    // console.log(offsetHeight + scrollTop, scrollHeight);
+    if (scrollHeight - (offsetHeight + scrollTop) < 20) {
+      this.clearTimeouts();
+      this.timeouts.push(setTimeout(this.doJob, 500));
+    }
+  };
+
+  doJob = () => {
+    console.log("it's at the end.");
+  };
+
+  clearTimeouts = () => {
+    this.timeouts.forEach(t => clearTimeout(t));
+    this.timeouts = [];
   };
 
   render() {
